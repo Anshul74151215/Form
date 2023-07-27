511f49faedaf887a1423dfb3cee7ad95b9c6fa01
Anshul ojha <ojhaanshul889@gmail.com>
2023-07-27 15:34:35 +0530

    first commit

--- app.js
+++ app.js
@@ -31,9 +31,3 @@ app.use("/api", formRoutes);
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Anshul74151215/Form_client.git
-git push -u origin main
