27b7012be4f2a9f3919316155eeb6dbd32d6d2bf
Anshul ojha <ojhaanshul889@gmail.com>
2023-07-28 10:55:26 +0530

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const form = require("./models/Form");
-const formRoutes = require("./routes/formRoutes");
+const formRoutes = require("./Routes/formRoutes");
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
